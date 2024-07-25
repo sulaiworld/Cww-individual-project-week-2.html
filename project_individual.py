83051da1898fb799de3de01fcc82b90437dab73d
sulaiworld <140712848+sulaiworld@users.noreply.github.com>
2024-07-25 13:49:32 +0100

    Add files via upload

---
+++ project_individual.py
@@ -0,0 +1,64 @@
+#Question 1 : Variable
+#Kindly remove multiline comments for the codes to run
+"""
+x = 10
+print(x)
+greetings = "Hello"
+print(greetings)
+name = "Sulaimon"
+print(name)
+print(len(name))
+print(type(name))
+"""
+
+#Question 2 : Datatypes
+"""
+y = 10
+z = 12.1
+school = "Unilorin"
+creator = True
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+print(school)
+print(len(school))
+print(type(school))
+print(creator)
+print(type(creator))
+"""
+
+#Question : 3 sets
+"""
+number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+number.add(11)
+number.remove(5)
+if 3 in number:
+    print(f"number 3 in set")
+print(len(number))
+"""
+
+#Question : 4 lists
+"""
+number = list(range(1, 11))
+number.append(11)
+number.remove(5)
+if 3 in number:
+    print(" number 3 is in the list")
+number.pop(0)
+print(len(number))
+number.sort(reverse=True)
+print(number)
+"""
+
+#Question : 5 Dictionaries
+"""
+days_of_week = {"Monday":1, "Tuesday": 2, "Wednesday": 3, "Thursday":4, "Friday":6, "Saturday":7, "Sunday":8}
+days_of_week['Sunday'] = 7
+days_of_week.pop("Wednesday")
+del days_of_week["Wednesday"]
+if "Thursday" in days_of_week:
+    print("Thursday in set")
+print(days_of_week)
+print(len(days_of_week))
+"""
